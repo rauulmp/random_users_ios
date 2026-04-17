d62ecd250b7ad3b07a6c6f66f2c7ec1cf3b7c9b5
Raul Montoya Perez <raulomontoyaperez@gmail.com>
2026-04-17 17:01:01 +0200

    Initial project

---
+++ RandomUsers/RandomUsers/RandomUsersApp.swift
@@ -0,0 +1,17 @@
+//
+//  RandomUsersApp.swift
+//  RandomUsers
+//
+//  Created by Raul Montoya Perez on 17/4/26.
+//
+
+import SwiftUI
+
+@main
+struct RandomUsersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ RandomUsers/RandomUsersTests/RandomUsersTests.swift
@@ -0,0 +1,17 @@
+//
+//  RandomUsersTests.swift
+//  RandomUsersTests
+//
+//  Created by Raul Montoya Perez on 17/4/26.
+//
+
+import Testing
+@testable import RandomUsers
+
+struct RandomUsersTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
